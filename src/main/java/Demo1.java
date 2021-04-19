7f8da1c204dfedac8206eedb01c87786ff373fee
Zxd1127 <258zxd@sina.com>
2021-04-19 11:04:00 +0800

    123

---
+++ src/main/java/Demo1.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName: Demo1
+ * @description:
+ * @author: ZhangXd
+ * @create: 2021-04-19 11:00
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
